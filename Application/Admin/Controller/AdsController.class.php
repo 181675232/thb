fff917829f210bacefcf226475224b6f30b13b19
晗彬 <@jolly-PC>
2015-11-18 11:44:48 +0800

    1

--- Application/Admin/Controller/AdsController.class.php
+++ Application/Admin/Controller/AdsController.class.php
@@ -1,18 +1,18 @@
 <?php
 namespace Admin\Controller;
 
-class ActivityController extends CommonController {
+class AdsController extends CommonController {
 	
 	public function index(){
 		
-		$table = M('Activity'); // 实例化User对象
+		$table = M('ads'); // 实例化User对象
 		
 		//接收查询数据
 		if (IS_POST){
-			$post = I('post.');
-			$data['title'] = $post['keyword'];
-			$table = $table->where($data);		
-		}elseif (I('get.state')){
+			$keyword = I('get.keyword');
+			$data['title'] = array('like',"%{$keyword}%");
+			$table = $table->where($data);
+		}elseif (I('get.verify')){
 			$data = I('get.');
 			$table = $table->where($data);
 			$this->assign('verify',I('get.verify'));
@@ -21,35 +21,33 @@ class ActivityController extends CommonController {
 		$Page       = new \Think\Page($count,15);// 实例化分页类 传入总记录数和每页显示的记录数(25)
 		$show       = $Page->show();// 分页显示输出
 		// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-		$res = $table->where($data)
-		->join('left join t_city on t_city.pinyin = t_activity.city and t_city.province = t_activity.province')->field('t_activity.*,t_city.name as city')
-		->order('istop desc,isred desc,ord asc,create_ts desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+		$res = $table->where($data)	->order('istop desc,isred desc,ord asc,addtime desc')->limit($Page->firstRow.','.$Page->listRows)->select();
 		$this->assign('data',$res);// 赋值数据集
 		$this->assign('page',$show);// 赋值分页输出
 		$this->display(); // 输出模板			
 	}
 	
-// 	public function add(){
-// 		if (IS_POST){
-// 			$table = D('Activity');
-// 			if ($table->create()){
-// 				if ($table->add()){
-// 					alertLocation('添加会员成功！', '/Admin/Activity');
-// 				}else {
-// 					$this->error('添加失败！');
-// 				}
-// 			}else {
-// 				$this->error($table->getError());
-// 			}
+	public function add(){
+		if (IS_POST){
+			$table = D('ads');
+			if ($table->create()){
+				if ($table->add()){
+					alertLocation('添加会员成功！', '/Admin/Activity');
+				}else {
+					$this->error('添加失败！');
+				}
+			}else {
+				$this->error($table->getError());
+			}
 			
-// 		}
-// 		$this->display();
-// 	}
+		}
+		$this->display();
+	}
 	
 	public function edit(){
 		$id = I('get.id');
 		if (IS_POST){
-			$table = D('Activity');
+			$table = D('ads');
 			if ($table->create()){
 				$data = $table->create();
 				$data['create_ts'] = strtotime($data['create_ts']);
@@ -64,7 +62,7 @@ class ActivityController extends CommonController {
 			}
 				
 		}
-		$table = M('Activity');
+		$table = M('ads');
 		$data = $table->where("id = $id")->find();
 		$data['avail_day'] = explode(',', $data['avail_day']);
 		$this->assign($data);
@@ -73,7 +71,7 @@ class ActivityController extends CommonController {
 	
 	public function state(){
 		$data = I('get.');			
-		$table = M('Activity');
+		$table = M('ads');
 		if ($table->save($data)){
 			$this->redirect("/Admin/Activity");
 		}else {
@@ -83,7 +81,7 @@ class ActivityController extends CommonController {
 	
 	public function delete(){		
 		$post = implode(',',$_POST['id']);	
-		$table = M('Activity');
+		$table = M('ads');
 		$data = $table->delete($post);
 		if ($data){
 			echo '删除成功！';
@@ -94,7 +92,7 @@ class ActivityController extends CommonController {
 	
 	public function ajaxstate(){
 		$data = I('get.');
-		$table = M('Activity');
+		$table = M('ads');
 		if ($table->save($data)){
 			echo 1;
 		}else {
@@ -104,7 +102,7 @@ class ActivityController extends CommonController {
 	
 	public function ajax(){
 		if (!empty($_POST['param'])){
-			$table = M('Activity');
+			$table = M('ads');
 			$data[$_POST['name']] = $_POST['param'];
 			$return = $table->where($data)->find();
 			if ($return){
